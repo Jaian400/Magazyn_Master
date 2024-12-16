4491c1ffe599629bac8c4e9abd640f140acd5444
maciejon <maciek.olpinski@gmail.com>
2024-12-16 14:12:40 +0100

    dobre errory

--- djan/mainapp/views.py
+++ djan/mainapp/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.db import IntegrityError
 
 # INDEX -> STRONA GŁOWNA
 
@@ -33,6 +34,86 @@ def logowanie_view(request):
             return render(request, 'logowanie.html', {"error": "Niepoprawny e-mail lub hasło."})
     return render(request, 'logowanie.html')
 
+# def rejestracja_view(request):
+#     try:
+#         if request.method == "POST":
+#             email = request.POST.get("email")
+#             password = request.POST.get("password")
+#             repeat_password = request.POST.get("repeat_password")
+#             first_name = request.POST.get("first_name")
+#             last_name = request.POST.get("last_name")
+#             user = None
+        
+#             if len(password) <= 8:
+#                 return render(
+#                     request, 
+#                     'rejestracja.html', 
+#                     {
+#                         "error_length": "Hasło musi mieć ponad 8 znaków.",
+#                         "email": email,
+#                         "first_name": first_name,
+#                         "last_name": last_name
+#                     }
+#                 )
+
+#             elif not any(char.isdigit() for char in password):
+#                 return render(
+#                     request,
+#                     'rejestracja.html',
+#                     {
+#                         "error_number": "Hasło musi zawierać cyfrę.",
+#                         "email": email,
+#                         "first_name": first_name,
+#                         "last_name": last_name
+#                     }
+#                     )
+            
+#             elif not any(char.isupper() for char in password):
+#                 return render(
+#                     request,
+#                     'rejestracja.html',
+#                     {
+#                         "error_capital_letter": "Hasło musi zawierać wielką literę.",
+#                         "email": email,
+#                         "first_name": first_name,
+#                         "last_name": last_name
+#                     }
+#                     )
+
+#             else:
+#                 if (password==repeat_password):
+#                     user = User.objects.create_user(
+#                         username=email.split('@')[0],  # nazwa uzytkownika generowana z maila
+#                         email=email,
+#                         password=password,
+#                         first_name=first_name,
+#                         last_name=last_name,
+#                     )
+#                 else:
+#                     return render(
+#                         request,
+#                         'rejestracja.html',
+#                         {
+#                             "error_repeat": "Hasła nie są takie same.",
+#                             "email": email,
+#                             "first_name": first_name,
+#                             "last_name": last_name
+#                         }
+#                     )
+#             if user is not None:
+#                 login(request, user)
+#                 return render(request, 'index.html')
+#     except :
+#         return render(
+#             request, 'rejestracja.html',
+#                 {
+#                     "error_email_taken": "Użytkownik o takim adresie email już istnieje.",
+#                     "first_name": first_name,
+#                     "last_name": last_name 
+#                 }
+#         )
+#     return render(request, 'rejestracja.html')
+
 def rejestracja_view(request):
     try:
         if request.method == "POST":
@@ -42,67 +123,50 @@ def rejestracja_view(request):
             first_name = request.POST.get("first_name")
             last_name = request.POST.get("last_name")
             user = None
+            error_length = None
+            error_number = None
+            error_capital_letter = None
+            error_repeat = None
         
             if len(password) <= 8:
-                return render(
-                    request, 
-                    'rejestracja.html', 
-                    {
-                        "error": "Hasło musi mieć ponad 8 znaków.",
-                        "email": email,
-                        "first_name": first_name,
-                        "last_name": last_name
-                    }
+                error_length = "Hasło musi mieć ponad 8 znaków."
+
+            if not any(char.isdigit() for char in password):
+                error_number = "Hasło musi zawierać cyfrę."
+
+            if not any(char.isupper() for char in password):
+                error_capital_letter = "Hasło musi zawierać wielką literę."
+
+            if not password==repeat_password:
+                error_repeat = "Hasła nie są takie same."
+
+            if not (error_length or error_number or error_capital_letter or error_repeat):
+                user = User.objects.create_user(
+                    username=email.split('@')[0],  # nazwa uzytkownika generowana z maila
+                    email=email,
+                    password=password,
+                    first_name=first_name,
+                    last_name=last_name,
                 )
 
-            elif not any(char.isdigit() for char in password):
+            if error_length or error_number or error_capital_letter or error_repeat:
                 return render(
-                    request,
-                    'rejestracja.html',
+                request,
+                'rejestracja.html',
                     {
-                        "error": "Hasło musi zawierać cyfrę.",
-                        "email": email,
-                        "first_name": first_name,
-                        "last_name": last_name
+                    "error_length": error_length,
+                    "error_number": error_number,
+                    "error_capital_letter": error_capital_letter,
+                    "error_repeat": error_repeat,
+                    "email": email,
+                    "first_name": first_name,
+                    "last_name": last_name
                     }
-                    )
-            
-            elif not any(char.isupper() for char in password):
-                return render(
-                    request,
-                    'rejestracja.html',
-                    {
-                        "error": "Hasło musi zawierać wielką literę.",
-                        "email": email,
-                        "first_name": first_name,
-                        "last_name": last_name
-                    }
-                    )
-
-            else:
-                if (password==repeat_password):
-                    user = User.objects.create_user(
-                        username=email.split('@')[0],  # nazwa uzytkownika generowana z maila
-                        email=email,
-                        password=password,
-                        first_name=first_name,
-                        last_name=last_name,
-                    )
-                else:
-                    return render(
-                        request,
-                        'rejestracja.html',
-                        {
-                            "error": "Hasła nie są takie same.",
-                            "email": email,
-                            "first_name": first_name,
-                            "last_name": last_name
-                        }
-                    )
+                )
             if user is not None:
                 login(request, user)
                 return render(request, 'index.html')
-    except :
+    except IntegrityError:
         return render(
             request, 'rejestracja.html',
                 {
